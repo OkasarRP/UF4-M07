5f9201332a910f38a98b77cc4308412ad6bc3a53
OkasarRP <oskyrp2000@gmail.com>
2024-05-07 18:48:38 +0200

    more errors

--- app/Http/Controllers/api/ProviderController.php
+++ app/Http/Controllers/api/ProviderController.php
@@ -27,18 +27,18 @@ class ProviderController extends Controller
     function create(Request $request){
         $provider = Provider::create($request->all());
         if($provider){
-            return response()->json(['povider'=>'Se ha insertado correctamente'],200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'Se ha insertado correctamente'],200, [], JSON_UNESCAPED_UNICODE);
         }else{
-            return response()->json(['povider'=>'No se ha podido insertar'],404, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'No se ha podido insertar'],404, [], JSON_UNESCAPED_UNICODE);
         }
     }
 
     function show($id){
         $provider = Provider::find($id);
         if($provider){
-            return response()->json(['povider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
         }else{
-            return response()->json(['povider'=>'no product finded'],404, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'no product finded'],404, [], JSON_UNESCAPED_UNICODE);
         }
     }
 
@@ -46,26 +46,26 @@ class ProviderController extends Controller
         $provider = Provider::find($id);
         if($provider){
             $provider->update($request->all());
-            return response()->json(['povider'=>$product],200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>$product],200, [], JSON_UNESCAPED_UNICODE);
         }else{
-            return response()->json(['povider'=>'no product'],404, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'no product'],404, [], JSON_UNESCAPED_UNICODE);
         }
     }
     function destroy($id){
         $provider = Provider::find($id);
         if($provider){
             $provider->delete();
-            return response()->json(['povider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
         }else{
-            return response()->json(['povider'=>'no product'],404, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'no product'],404, [], JSON_UNESCAPED_UNICODE);
         }
     }
     function edit($id){
         $provider = Provider::find($id);
         if($provider){
-            return response()->json(['povider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>$provider],200, [], JSON_UNESCAPED_UNICODE);
         }else{
-            return response()->json(['povider'=>'no povider'],404, [], JSON_UNESCAPED_UNICODE);
+            return response()->json(['provider'=>'no provider'],404, [], JSON_UNESCAPED_UNICODE);
         }
     }
 
