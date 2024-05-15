16f9c55c3a2846fa7cfdf9f1f1323aee8d4ae219
OkasarRP <oskyrp2000@gmail.com>
2024-05-15 19:02:22 +0200

    asdfasdf

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use Illuminate\Support\Facades\
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
