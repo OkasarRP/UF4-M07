0cdd98d11a1b007309553a691378c493043dd3fe
OkasarRP <oskyrp2000@gmail.com>
2024-05-21 18:53:03 +0200

    asdfasdf

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string',
             'email' => 'required|email|unique:users,email',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:1',
             'password_confirmation' => 'required|same:password'
         ]);
 
